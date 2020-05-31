006161f9ccf1cf7e9917fea2467406e63627790c
enjoy <enjoy@MacBook-Pro-enjoy.local>
2020-05-31 16:51:59 +0300

    First commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Profile () {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://html5css.ru/css/img_forest.jpg" />
+            </div>
+            <div>
+                ava +desc
+            </div>
+            <div>
+                My posts
+                <div>
+                    Yor post typing
+                </div>
+            </div>
+            <div>
+                <div>
+                    Post1
+                </div>
+                <div>
+                    Post2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
